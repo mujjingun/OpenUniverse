2fe670ef111fdaa6a7ad5d94a364beb17fea0a6a
Gun Park <mujjingun@gmail.com>
2018-09-20 14:26:51 -0400

    use 5x5 digits

--- shadersrc/numbers.frag.glsl
+++ shadersrc/numbers.frag.glsl
@@ -12,74 +12,96 @@ layout(location = 0) out vec4 outColor;
 #define CHAR_HEIGHT 20
 #define SPACE 5
 
-float figures[10][3][3] = float[][][](
+float figures[10][5][5] = float[][][](
     float[][](
-        float[](1, 1, 1),
-        float[](1, 0, 1),
-        float[](1, 1, 1)
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 1, 0),
-        float[](0, 1, 0),
-        float[](1, 1, 1)
+        float[](1, 1, 1, 0, 0),
+        float[](0, 0, 1, 0, 0),
+        float[](0, 0, 1, 0, 0),
+        float[](0, 0, 1, 0, 0),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 1, 0),
-        float[](0, 1, 0),
-        float[](0, 1, 1)
+        float[](1, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 0),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 1, 1),
-        float[](0, 1, 1),
-        float[](1, 1, 1)
+        float[](1, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 0, 1),
-        float[](1, 1, 1),
-        float[](0, 0, 1)
+        float[](1, 0, 0, 0, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](0, 0, 0, 0, 1)
     ),
     float[][](
-        float[](0, 1, 1),
-        float[](0, 1, 0),
-        float[](1, 1, 0)
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 0),
+        float[](1, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 0, 0),
-        float[](1, 1, 1),
-        float[](1, 1, 1)
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 0),
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 1, 1),
-        float[](0, 0, 1),
-        float[](0, 0, 1)
+        float[](0, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](0, 0, 0, 1, 0),
+        float[](0, 0, 1, 0, 0),
+        float[](0, 0, 1, 0, 0)
     ),
     float[][](
-        float[](1, 1, 1),
-        float[](1, 1, 1),
-        float[](1, 1, 1)
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1)
     ),
     float[][](
-        float[](1, 1, 1),
-        float[](1, 1, 1),
-        float[](0, 0, 1)
+        float[](1, 1, 1, 1, 1),
+        float[](1, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1),
+        float[](0, 0, 0, 0, 1),
+        float[](1, 1, 1, 1, 1)
     )
 );
 
 void main() {
     vec2 coord = gl_FragCoord.xy;
 
-    if (coord.x < (CHAR_WIDTH + SPACE) * 3 && coord.y < CHAR_HEIGHT) {
-        coord.x /= (CHAR_WIDTH + SPACE);
+    if (coord.x > SPACE && coord.x < (CHAR_WIDTH + SPACE) * 3 + SPACE && coord.y < CHAR_HEIGHT) {
+        coord.x = (coord.x - SPACE) / (CHAR_WIDTH + SPACE);
         coord.y /= CHAR_HEIGHT;
 
         int index = int(coord.x);
         coord.x = mod(coord.x, 1) * 1.2;
 
         uint digit = digits[index].n;
-        int x = int(coord.x * 3);
-        int y = int(coord.y * 3);
+        int x = int(coord.x * 5);
+        int y = int(coord.y * 5);
 
-        outColor = vec4(vec3(x >= 3 ? 0.0 : figures[digit][y][x]), 1.0f);
+        if (x >= 5) discard;
+
+        outColor = vec4(vec3(figures[digit][y][x]), 1.0f);
         return;
     }
 
